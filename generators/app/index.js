648897bdc004bb659d65e1cd29555fd4561b34a5
arrawatia <sumit.arrawatia@gmail.com>
2015-06-03 11:57:30 -0700

    Initial commit.

---
+++ generators/app/index.js
@@ -0,0 +1,72 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var mkdirp = require('mkdirp');
+var wrench = require('wrench');
+
+module.exports = yeoman.generators.Base.extend({
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the outstanding ' + chalk.red('Rico') + ' generator!'
+    ));
+
+    var prompts = [];
+    // {
+    //         name: 'appName',
+    //         message: 'What is your app\'s name ?'
+    //     }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+      this.appName = props.appName;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      
+      console.log("Creating a scafold in the current dir....");
+
+      // mkdirp("bin");
+      this.directory(this.templatePath(), ".");
+
+      // mkdirp("app");
+      // mkdirp("config");
+      // mkdirp("lib");
+      // this.directory(this.templatePath("bin"), "bin");
+
+
+      // this.fs.copy(
+      //   this.templatePath('_package.json'),
+      //   this.destinationPath('package.json')
+      // );
+      // this.fs.copy(
+      //   this.templatePath('_bower.json'),
+      //   this.destinationPath('bower.json')
+      // );
+    }
+
+    
+  },
+
+  install: function () {
+    // this.installDependencies();
+     var howToInstall =
+      '\nPlease run ' +
+      chalk.yellow.bold('pip install -r bin/requirements.txt') +
+      ' to install the rico dependencies.' +
+      '\nThen run the following to setup the rico enviornment:' +
+      '\n' + chalk.yellow.bold('source bin/set_env.bash') +
+      '\n' +chalk.yellow.bold('bin/rico build jar') ;
+
+      this.log(howToInstall);
+
+  }
+});
