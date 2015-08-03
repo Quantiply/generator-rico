b21bfbabd99093523981b43424ce953d1561655c
Roger Hoover <roger.hoover@gmail.com>
2015-08-02 23:50:27 -0700

    Create and load topic scripts

---
+++ generators/app/templates/bin/load_topics.sh
@@ -0,0 +1,6 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR=${DIR}/..
+
+# ${ROOT_DIR}/deploy/confluent/bin/kafka-console-producer \
+#               --broker-list localhost:8092 --topic example \
+#               --compression-codec lz4 --new-producer < data/example.json
