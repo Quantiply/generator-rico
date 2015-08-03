986c3d904c0bda57d9f00d66e790fd161cfd3964
Roger Hoover <roger.hoover@gmail.com>
2015-08-03 00:02:00 -0700

    Fixed port

--- generators/app/templates/bin/load_topics.sh
+++ generators/app/templates/bin/load_topics.sh
@@ -2,5 +2,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=${DIR}/..
 
 # ${ROOT_DIR}/deploy/confluent/bin/kafka-console-producer \
-#               --broker-list localhost:8092 --topic example \
-#               --compression-codec lz4 --new-producer < data/example.json
+#               --broker-list localhost:9092 --topic example \
+#               --compression-codec lz4 --new-producer < ${ROOT_DIR}/data/example.json
